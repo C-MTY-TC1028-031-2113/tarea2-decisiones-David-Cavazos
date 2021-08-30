0c324c2351f6d131e4630a0e829a7ce4a80dc406
user <A01721909@itesm.mx>
2021-08-29 21:11:09 -0500

    Ago292021

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,18 +1,23 @@
 
 def main():
     edad = int(input("Ingresa tu edad: "))
-    identificacion = input("¿Tienes identificación oficial? (s/n): ")
-
-
-    if((identificacion != "s" and identificacion !="n") or (edad<0)):
-        print("Respuesta incorrecta")
-    
-
-    elif(edad>=18 and identificacion=="s"):
-        print("Trámite de licencia concedido")
-
-    elif(edad<=18 or identificacion=="n"):
+    if(edad<18 ):
         print("No cumples requisitos")
+    else: 
+        identificacion = input("¿Tienes identificación oficial? (s/n): ")
+
+
+        if((identificacion != "s" and identificacion !="n") or (edad<0)):
+            print("Respuesta incorrecta")
+        
+        
+
+        elif(edad>=18 and identificacion=="s"):
+            print("Trámite de licencia concedido")
+        else:
+            print("No cumples requisitos")
+
+    
 
 
 if __name__ == '__main__':
